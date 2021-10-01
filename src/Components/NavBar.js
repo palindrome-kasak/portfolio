dee3d7f21703740803b8715eab08d0c784641b59
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2021-10-01 00:30:09 -0700

    navbar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+
+const NavBar = () => {
+    return (
+        <>
+            <div>
+                <ul>
+                    <li>
+                    <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                    <NavLink to="/About">About me</NavLink>
+                    </li>
+                    <li>
+                    <NavLink to="/Projects">Projects</NavLink>
+                    </li>
+                    <li>
+                    <NavLink to="/Contact">Contact</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default NavBar

ed0e2ba9835955c99d56f13adb165e10dc8ea6e3
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2021-10-12 23:43:11 -0700

    new project added

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 // import CoinTrackerImg from '../images/cointracker.jpg';
 // import CavinImg from '../images/cavinimg.jpg';
 import covid from '../pictures/covid.png'
+import Contango from '../pictures/Contango.jpg'
 
 const projects = [
   {
@@ -13,35 +14,37 @@ const projects = [
     desc:
       'A web application which provides all updated  Covid19 resources and information of all over india.Developed all UI using reactjs and an library material ui and build all UI/UX designs in figma. Developed seperated section for each resources like oxygen, ambulance etc for easily accessible. Add firebase for dynamic data ,which helps in updating, deleting adding data.',
     img: covid,
+    url:'https://optimistic-spence-85de64.netlify.app/'
   },
   {
     id: uuidv4(),
-    name: 'Green CTG',
+    name: 'Contango',
     desc:
-      'An app to help people to get an overview of how they can make the city beautiful.',
-    // img: GreenCtgImg,
+      'Build a React-native expo App for grow your investment through playing game with other players. Developed wireframes and screens using REACT NATIVE frame work. Learn and use navigator to provide navigation through out the app.',
+     img: Contango,
+     url:'https://play.google.com/store/apps/details?id=com.contango.contango',
   },
-  {
-    id: uuidv4(),
-    name: 'Coin Tracker',
-    desc:
-      'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
-    // img: CoinTrackerImg,
-  },
-  {
-    id: uuidv4(),
-    name: "Cavin's Portfolio",
-    desc:
-      'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
-    // img: CavinImg,
-  },
-  {
-    id: uuidv4(),
-    name: 'Tracking Soft',
-    desc:
-      'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
-    // img: ProjectImg,
-  },
+  // {
+  //   id: uuidv4(),
+  //   name: 'Coin Tracker',
+  //   desc:
+  //     'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
+  //   // img: CoinTrackerImg,
+  // },
+  // {
+  //   id: uuidv4(),
+  //   name: "Cavin's Portfolio",
+  //   desc:
+  //     'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
+  //   // img: CavinImg,
+  // },
+  // {
+  //   id: uuidv4(),
+  //   name: 'Tracking Soft',
+  //   desc:
+  //     'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
+  //   // img: ProjectImg,
+  // },
 ];
 
 export default projects;
